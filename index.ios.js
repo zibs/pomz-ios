7ba25de71e6bf2b84902cf3c7cbc8eb3a60fb7ee
zibs <elizibin@gmail.com>
2016-03-18 09:58:34 -0700

    Implement CountDownPoem component. Add object with number/name key value to make message more readable

--- index.ios.js
+++ index.ios.js
@@ -38,6 +38,11 @@ const styles = StyleSheet.create({
    height: 350,
    width: 300,
  },
+ poemWaiter: {
+   backgroundColor: "rgba(104, 185, 155, 0.7)",
+   lineHeight: 50,
+   fontSize: 30,
+ },
 });
 
 var radio_props = [
@@ -80,7 +85,6 @@ class RadioButtonSignUpForm extends Component {
         return this.props.renderNewView(<WaitForPoem renderNewView={this.props.renderNewView } />);
       })
       .catch((error) => {
-        // display error component
         console.warn(error);
       });
    }
@@ -90,7 +94,6 @@ class RadioButtonSignUpForm extends Component {
       <Radio
           radio_props={radio_props}
           initial={0}
-          /*onPress={(value) => {this.setState({value:value});}}*/
           onPress={ (value) => {this.signUpToApp(value);} }
         />
     );
@@ -119,6 +122,41 @@ class RequestPushNotifications extends Component {
     );
   }
 }
+class CountDownPoem extends Component {
+  constructor(props){
+    super(props);
+    this.state = {hours: 0, minutes: 0, seconds: 0 };
+  }
+  tick() {
+    let hoursTillMidnight =  numToWord[(24 - (new Date().getHours())).toString()];
+    let minutesInHour =  numToWord[(60 - (new Date().getMinutes())).toString()];
+    let secondsInMinute =  numToWord[(60 - (new Date().getSeconds())).toString()];
+    this.setState({
+        hours: hoursTillMidnight,
+        minutes: minutesInHour,
+        seconds: secondsInMinute
+      });
+  }
+  componentDidMount() {
+    this.interval = setInterval(function() {
+                      this.tick();
+                    }.bind(this), 1000);
+    }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  render() {
+    return(
+      <Text style={styles.poemWaiter}>a nu 1 in {"\n"}
+            {this.state.hours} hours {"\n"}
+            {this.state.minutes} minutes {"\n"}
+            {this.state.seconds} seconds {"\n"}
+      </Text>
+    );
+  }
+}
+
+
 class PoemView extends Component {
   constructor(props) {
     super(props);
@@ -157,7 +195,7 @@ class iozpomz extends Component {
     // display poem on cold-launch
     const notification = PushNotificationIOS.popInitialNotification();
     if (notification){
-      this.setState({mainView: <PoemView poemUrl={notification._data.poem_url}/>});
+      this.setState({mainView: <PoemView poemUrl={notification._data.poem_url} renderNewView={this.renderNewView.bind(this)}/>});
     }
     // check if permissions are enabled for push.
     PushNotificationIOS.checkPermissions(function(permissions){
@@ -172,6 +210,8 @@ class iozpomz extends Component {
         }.bind(this));
       }
     }.bind(this));
+    // finally, display countdown poem page since the user has already seen the poem.
+    this.setState({mainView: <CountDownPoem />});
   }
   render() {
     return (
@@ -183,6 +223,70 @@ class iozpomz extends Component {
 }
 
 AppRegistry.registerComponent('iozpomz', () => iozpomz, 'RadioButtonSignUpForm', () => RadioButtonSignUpForm, 'RequestPushNotifications', () => RequestPushNotifications, 'PoemView', () => PoemView);
+
+const numToWord = {
+      0: "zero",
+       1: "one",
+       2: "two",
+       3: "three",
+       4: "four",
+       5: "five",
+       6: "six",
+       7: "seven",
+       8: "eight",
+       9: "nine",
+       10: "ten",
+       11: "eleven",
+       12: "twelve",
+       13: "thirteen",
+       14: "fourteen",
+       15: "fifteen",
+       16: "sixteen",
+       17: "seventeen",
+       18: "eighteen",
+       19: "nineteen",
+       20: "twenty",
+       21: "twenty-one",
+       22: "twenty-two",
+       23: "twenty-three",
+       24: "twenty-four",
+       25: "twenty-five",
+       26: "twenty-six",
+       27: "twenty-seven",
+       28: "twenty-eight",
+       29: "twenty-nine",
+       30: "thirty",
+       31: "thirty-one",
+       32: "thirty-two",
+       33: "thirty-three",
+       34: "thirty-four",
+       35: "thirty-five",
+       36: "thirty-six",
+       37: "thirty-seven",
+       38: "thirty-eight",
+       39: "thirty-nine",
+       40: "forty",
+       41: "forty-one",
+       42: "forty-two",
+       43: "forty-three",
+       44: "forty-four",
+       45: "forty-five",
+       46: "forty-six",
+       47: "forty-seven",
+       48: "forty-eight",
+       49: "forty-nine",
+       50: "fifty",
+       51: "fifty-one",
+       52: "fifty-two",
+       53: "fifty-three",
+       54: "fifty-four",
+       55: "fifty-five",
+       56: "fifty-six",
+       57: "fifty-seven",
+       58: "fifty-eight",
+       59: "fifty-nine",
+       60: "sixty"
+};
 
 // { _data: { pom_url: 'http://localhost:3000/api/v1/poems/03-16-2016' },
 //   _alert: 'a pom 4 u',
